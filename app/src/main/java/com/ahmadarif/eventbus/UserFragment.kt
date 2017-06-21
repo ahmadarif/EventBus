81adc8419a3d99174a2bfd002b7e1ce1601d2a44
Ahmad Arif <ahmad.arif019@gmail.com>
2017-06-21 10:22:14 +0700

    completed UserFragment

--- app/src/main/java/com/ahmadarif/eventbus/UserFragment.kt
+++ app/src/main/java/com/ahmadarif/eventbus/UserFragment.kt
@@ -5,6 +5,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_user.*
+import org.greenrobot.eventbus.Subscribe
+import android.widget.EditText
+
+
+
 
 class UserFragment : Fragment() {
 
@@ -13,8 +20,31 @@ class UserFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        GlobalBus.bus.register(this)
+
         // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_user, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSubmit.setOnClickListener {
+            // We are broadcasting the message here to listen to the subscriber.
+            val fragmentActivityMessageEvent = Events.FragmentActivityMessage(editMessage.text.toString())
+            GlobalBus.bus.post(fragmentActivityMessageEvent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        GlobalBus.bus.unregister(this)
+    }
+
+    @Subscribe
+    fun getMessage(message: Events.ActivityFragmentMessage) {
+        txtMessage.text = "${getString(R.string.message_received)} ${message.message}"
+        Toast.makeText(activity, "${getString(R.string.message_fragment)} ${message.message}", Toast.LENGTH_SHORT).show()
     }
 
 }
